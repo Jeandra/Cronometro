51f86d377cfe41c94e5873204434659dc7817307
Jeandra <jeandra8789@gmail.com>
2023-04-11 21:10:25 -0300

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -4,18 +4,18 @@ let [hours,minutes,seconds] = [0,0,0];
 
 function container(){
     seconds++;
-    if(seconds==60){
-        seconds=0;
+    if(seconds == 60){
+        seconds = 0;
         minutes++;
-        if(minutes==60){
-            minutes=0;
+        if(minutes == 60){
+            minutes = 0;
             hours++;
         }
     }
 
-    let h = hours<10 ? ("0"+hours) : hours;
-    let m = minutes<10 ? ("0"+minutes) : minutes;
-    let s = seconds<10 ? ("0"+seconds) : seconds;
+    let h = hours < 10 ? ("0" + hours) : hours;
+    let m = minutes < 10 ? ("0" + minutes) : minutes;
+    let s = seconds < 10 ? ("0" + seconds) : seconds;
 
     timeEl.innerHTML = h+":"+m+":"+s;
 }
